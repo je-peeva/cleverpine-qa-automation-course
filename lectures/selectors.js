1f9cf5685d8e2922aa98b5b2487840ea4627d649
Zhenya Peeva <jenny.radulova@gmail.com>
2025-11-03 09:53:13 +0200

    feat(task01-03): add constants.js and selectors.js used for all tasks

---
+++ lectures/selectors.js
@@ -0,0 +1,18 @@
+export const selectors = {
+  //Buttons:
+  signInButton: "#sign-in-button",
+
+  //Links:
+  loginLink: "#nav-link-login",
+  profileLink: "#nav-link-profile",
+
+  //Inputs:
+  usernameEmailInput: "#defaultLoginFormUsername",
+  passwordInput: "#defaultLoginFormPassword",
+
+  //Headings:
+  profileHeading: ".col-12 h2",
+
+  //Toasters:
+  toastContainer: "#toast-container",
+};
